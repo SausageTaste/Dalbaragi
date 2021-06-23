8366cb0e19678cdaedd5225a93a96d2ef202abc0
Sungmin Woo <woos8899@gmail.com>
2021-06-24 03:15:37 +0900

    Light matrix creation function was wrong

--- lib/renderer/d_render_cpnt.cpp
+++ lib/renderer/d_render_cpnt.cpp
@@ -13,7 +13,7 @@ namespace dal {
         auto proj_mat = glm::ortho<float>(
             -half_proj_box_edge_length, half_proj_box_edge_length,
             -half_proj_box_edge_length, half_proj_box_edge_length,
-            -half_proj_box_edge_length * 10, half_proj_box_edge_length - 10
+            -half_proj_box_edge_length * 10, half_proj_box_edge_length * 10
         );
         proj_mat[1][1] *= -1;
 
